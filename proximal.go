78bc1dae183315e924d8ed5b3b7a027eae47b239
a-poor <45295232+a-poor@users.noreply.github.com>
2021-09-27 16:06:07 -0700

    Adding starter code

--- proximal.go
+++ proximal.go
@@ -1,4 +1,31 @@
 package proximal
 
-type Proximal struct {
+type Proxy struct {
+	idx       int
+	Upstreams []string
+}
+
+func (p *Proxy) AddUpstream(up string) {
+	if p.Upstreams == nil {
+		p.Upstreams = make([]string, 1)
+	}
+	p.Upstreams = append(p.Upstreams, up)
+}
+
+func (p *Proxy) incUpstream() {
+	p.idx = (p.idx + 1) % len(p.Upstreams)
+}
+
+func (p *Proxy) getUpstream() string {
+	return p.Upstreams[p.idx]
+}
+
+func (p *Proxy) GetNextUpstream() string {
+	up := p.getUpstream()
+	p.incUpstream()
+	return up
+}
+
+func (p *Proxy) Run() error {
+	return nil
 }
